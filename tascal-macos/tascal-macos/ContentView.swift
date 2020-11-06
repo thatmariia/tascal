86ba5edae290a3e6dfd3b6107c2a3f31374a0de1
Mariia Turchina <mariia.turchina@gmail.com>
2020-11-06 22:19:13 +0100

    new init commit lol

---
+++ tascal-macos/tascal-macos/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  tascal-macos
+//
+//  Created by Mariia Turchina on 06/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
