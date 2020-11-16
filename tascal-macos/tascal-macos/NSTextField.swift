8d8bf6aa6147311b038bae9226df68ad80f52688
Mariia Turchina <mariia.turchina@gmail.com>
2020-11-16 01:51:18 +0100

    focus ring and small refinements

--- tascal-macos/tascal-macos/NSTextField.swift
+++ tascal-macos/tascal-macos/NSTextField.swift
@@ -2,15 +2,7 @@
 //  NSTextField.swift
 //  tascal-macos
 //
-//  Created by Mariia Turchina on 08/11/2020.
+//  Created by Mariia Turchina on 16/11/2020.
 //
 
 import Foundation
-import SwiftUI
-
-extension NSTextField {
-    open override var focusRingType: NSFocusRingType {
-        get { .none }
-        set { }
-    }
-}
