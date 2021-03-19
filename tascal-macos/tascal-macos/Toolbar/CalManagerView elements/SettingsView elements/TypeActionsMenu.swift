212ee1b710a6214906b0206aaed5e881a9aa233e
Mariia Turchina <mariia.turchina@gmail.com>
2021-03-20 00:09:35 +0100

    fixed type deletion ordering

--- tascal-macos/tascal-macos/Toolbar/CalManagerView elements/SettingsView elements/TypeActionsMenu.swift
+++ tascal-macos/tascal-macos/Toolbar/CalManagerView elements/SettingsView elements/TypeActionsMenu.swift
@@ -58,9 +58,8 @@ struct TypeActionsMenu: View {
                 Button(action: {
                     let lvl = tt.level
                     
+                    handle_type_deletion(at: lvl)
                     delete_type(tt: tt)
-                    push_down_tasks(lvl: lvl)
-                    push_down_types(lvl: lvl)
                     
                     showing = false
                     
@@ -88,6 +87,65 @@ struct TypeActionsMenu: View {
             case .success(let new_task_type):
                 self.task_types.types.insert(new_task_type, at: new_task_type.level-1)//append(new_task_type)
                 print("Successfully added item")
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    fileprivate func handle_type_deletion(at lvl: Int) {
+        let last_type_lvl = task_types.types.count
+        
+        for tt in task_types.types {
+            if tt.level > lvl {
+                _modify_type(tt: tt, to: tt.level-1)
+            }
+        }
+
+        for task in tasks.all_tasks {
+            if task.level == lvl {
+                _update_task(task: task, to: last_type_lvl-1)
+            } else if task.level > lvl {
+                _update_task(task: task, to: task.level-1)
+            }
+        }
+        
+    }
+    
+    fileprivate func _modify_type(tt: TaskType, to lvl: Int) {
+        var updated_tt = tt
+        updated_tt.level = lvl
+        
+        CloudKitHelper.modify_tasktypes(task_type: updated_tt) { (result) in
+            switch result {
+            case .success(let item):
+                for i in 0..<task_types.types.count {
+                    let currentItem = task_types.types[i]
+                    if currentItem.record_id == item.record_id {
+                        task_types.types[i] = item
+                    }
+                }
+                print("Successfully modified item")
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    fileprivate func _update_task(task: Task, to lvl: Int) {
+        var updated_task = task
+        updated_task.level = lvl
+        
+        CloudKitHelper.modify_tasks(task: updated_task) { (result) in
+            switch result {
+            case .success(let item):
+                for i in 0..<tasks.all_tasks.count {
+                    let currentItem = tasks.all_tasks[i]
+                    if currentItem.record_id == item.record_id {
+                        tasks.all_tasks[i] = item
+                    }
+                }
+                print("Successfully modified item")
             case .failure(let err):
                 print(err.localizedDescription)
             }
@@ -162,59 +220,7 @@ struct TypeActionsMenu: View {
             }
         }
     }
-    
-    fileprivate func push_down_tasks(lvl: Int) {
-        if lvl == 1 { return }
-        
-        for task in tasks.all_tasks {
-            if task.level >= lvl {
-                var updated_task = task
-                updated_task.level = task.level - 1
-                
-                CloudKitHelper.modify_tasks(task: updated_task) { (result) in
-                    switch result {
-                    case .success(let item):
-                        for i in 0..<tasks.all_tasks.count {
-                            let currentItem = tasks.all_tasks[i]
-                            if currentItem.record_id == item.record_id {
-                                tasks.all_tasks[i] = item
-                            }
-                        }
-                        print("Successfully modified item")
-                    case .failure(let err):
-                        print(err.localizedDescription)
-                    }
-                }
-                
-            }
-        }
-    }
-    
-    fileprivate func push_down_types(lvl: Int) {
-        if lvl == task_types.types.count { return }
-        
-        for task_type in task_types.types {
-            if task_type.level > lvl {
-                var updated_type = task_type
-                updated_type.level = task_type.level - 1
-                
-                CloudKitHelper.modify_tasktypes(task_type: updated_type) { (result) in
-                    switch result {
-                    case .success(let item):
-                        for i in 0..<task_types.types.count {
-                            let currentItem = task_types.types[i]
-                            if currentItem.record_id == item.record_id {
-                                task_types.types[i] = item
-                            }
-                        }
-                        print("Successfully modified item")
-                    case .failure(let err):
-                        print(err.localizedDescription)
-                    }
-                }
-            }
-        }
-    }
+
 }
 
 
